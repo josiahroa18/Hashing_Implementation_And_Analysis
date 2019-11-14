921b9d46f3661f86fe0c532f82bd77ec6c715dc1
Josiah Roa <josiahroa18@gmail.com>
2019-11-13 22:05:59 -0700

    updated due to testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "LLHashTable.hpp"
+#include "RBT.hpp"
 using namespace std;
 
 // TODO: add a boolean parameter for which hash function to use
@@ -7,14 +8,28 @@ using namespace std;
 
 int main(){
     int values[6] = {3, 13, 19, 7, 5, 6};
-    LLHashTable hashTable;
-    for(int i=0; i<6; i++){
-        cout << "Inserting: " << values[i] << endl;
-        hashTable.insertNode(values[i]);
-    }
-    hashTable.printHashTable();
-    hashTable.deleteNode(1);
-    hashTable.deleteNode(6);
-    hashTable.deleteNode(19);
-    hashTable.printHashTable();
+
+    // LL Hash Table Collision Resolution Implementation
+    // LLHashTable hashTable;
+    // for(int i=0; i<6; i++){
+    //     cout << "Inserting: " << values[i] << endl;
+    //     hashTable.insertNode(values[i]);
+    // }
+    // hashTable.printHashTable();
+    // hashTable.deleteNode(1);
+    // hashTable.deleteNode(6);
+    // hashTable.deleteNode(19);
+    // hashTable.printHashTable();
+
+    // RBT Test Cases
+    RBT tree;
+    tree.insert(7);
+    tree.insert(6);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(5);
+    tree.insert(1);
+    tree.printRBT();
+
 }
